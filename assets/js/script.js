0e3e624db6bb620b55b29d819ed6574bc60643c4
Ed Hickey <bassnode@gmail.com>
2012-10-24 09:43:27 -0700

    Make cover fetching more reliable. Artist fallback.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,53 @@
-ARTWORK_URL = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=c1468b5d4e01bbb9afda80d93069f311&format=json";
+
+function apiURL(method, keyValuePairs) {
+  var url = "http://ws.audioscrobbler.com/2.0/";
+  var apiKey = "c1468b5d4e01bbb9afda80d93069f311";
+
+  if(method == 'album') {
+    var method = "album.getinfo";
+  } else {
+    var method = "artist.getinfo";
+  }
+
+  return url + '?format=json&callback=?&api_key=' + apiKey + '&' + queryString(keyValuePairs) + '&method=' + method;
+}
+
+function queryString(params) {
+  var output = [];
+  $.each(params, function(key, value){
+    if(params.hasOwnProperty(key)) {
+      output.push( key + '=' + encodeURIComponent(value) );
+    }
+  });
+
+  return output.join('&');
+}
+
+// Try to just get an artist image
+function setArtistCover(artist, element) {
+  $.getJSON(apiURL('artist', {artist: artist}), function(data){
+    if(data.artist) {
+      var img = data.artist.image[2]['#text'];
+      setCover(element, img);
+    }
+  });
+}
+
+function setCover(element, img) {
+  $('img.cover', element).attr('src', img);
+}
 
 // Load the image cover art in from Last.FM
-function getArtwork(element){
+function getArtwork(element) {
   var artist = $('.artist', element).text();
   var title = $('.title', element).text();
-  var url = ARTWORK_URL + "&&artist=" + encodeURIComponent(artist) + "&album=" + encodeURIComponent(title) + "&callback=?";
+  var params = { artist: artist, album: title };
 
-  $.getJSON(url, null, function(data){
+  $.getJSON(apiURL('album', params), function(data){
     if(data.album) {
       var img = data.album.image[2]['#text'];
-      $('img.cover', element).attr('src', img);
+      // Sometimes Last.fm returns empty strings for images...wtf?
+      img == '' ? setArtistCover(artist, element) : setCover(element, img);
 
       // Add the first track name if we can
       if(data.album.tracks.track) {
@@ -37,6 +75,9 @@ function getArtwork(element){
 
         $('.links', element).append(mouseOver);
       }
+    } else {
+      // The album query returned nothing, so try artist
+      setArtistCover(artist, element);
     }
   });
 }
